467e4d2fc95b74336e8a8da46c734da23f96feab
maxhodges <max@whiterabbitjapan.com>
2014-11-17 19:47:33 +0900

    Change load order of subscriptions so dropdown loads first.

--- client/_subscribe.js
+++ client/_subscribe.js
@@ -1,8 +1,7 @@
-
+Meteor.subscribe('kana');
 
 Meteor.autosubscribe(function(){
   Meteor.subscribe('wordsContainingPhonemes', Session.get('phoneme'), Session.get('page'));
 });
 
 
-Meteor.subscribe('kana');
